36d86bb71f2c5dd0b0edae13c060618ad9d39618
Chris Tate <chris@austincodeshop.com>
2021-12-19 04:41:59 -0600

    fixes mobile and updates hero text

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,30 +4,34 @@ const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
-        <div className={styles.copyright}>&copy; 2021 Austin Code Shop LLC</div>
-        <div>
-          <ul className={styles.socialList}>
-            <li className={styles.socialItem}>
-              <a
-                className={styles.github}
-                href="https://github.com/StarLedgerNFT"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                GitHub
-              </a>
-            </li>
-            <li className={styles.socialItem}>
-              <a
-                className={styles.twitter}
-                href="https://twitter.com/StarLedgerNFT"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Twitter
-              </a>
-            </li>
-          </ul>
+        <div className={styles.wrapper}>
+          <div className={styles.copyright}>
+            &copy; 2021 Austin Code Shop LLC
+          </div>
+          <div>
+            <ul className={styles.socialList}>
+              <li className={styles.socialItem}>
+                <a
+                  className={styles.github}
+                  href="https://github.com/StarLedgerNFT"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li className={styles.socialItem}>
+                <a
+                  className={styles.twitter}
+                  href="https://twitter.com/StarLedgerNFT"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Twitter
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </footer>
     </>
--- components/hero.tsx
+++ components/hero.tsx
@@ -13,8 +13,7 @@ const Hero = () => {
         <div className={styles.stars3}></div>
         <div className={styles.text}>
           <h2>
-            Limited NFT collection of 5,000 stars that represent stars in our
-            own galaxy. On the Ethereum blockchain. Powered by $METIS.
+            Buy a star. On Ethereum.
           </h2>
         </div>
       </div>
